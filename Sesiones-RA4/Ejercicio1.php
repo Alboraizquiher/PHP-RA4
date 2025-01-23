c0cd60410cf430011df98debe4bc9e179b8f5660
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2025-01-23 09:53:47 +0100

    edit exercise 1

--- Sesiones-RA4/Ejercicio1.php
+++ Sesiones-RA4/Ejercicio1.php
@@ -6,3 +6,43 @@ b) Se debe poder añadir y quitar leche o refresco seleccionando de una lista
 c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese
 caso mostrar error.
  -->
+
+ <?php
+// when user click on a button(if form POST has been submitted )
+        // get data from form
+
+        // save worker data
+        // detect button
+
+        //detect button
+            // to add products
+                //evaluate product
+                // add quantity to corresponding product
+
+            //to remove products
+                //evaluate product
+                // check if quantity is not greater than current one
+                // substract from quantity to corresponding product
+
+
+session_start(); 
+
+
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+
+ <head>
+
+<title>Supermarket management</title>
+
+ </head>
+
+ <body>
+    <h1>Supermarket management</h1>
+<!-- form with 3 buttons -->
+<!-- list values worjer,milk,softdrink... -->
+ </body>
+
+ </html>
