f7643137cac754fdac91f3585584558d293cf3b9
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2025-01-23 09:29:55 +0100

    Create new exercises

---
+++ Sesiones-RA4/Ejercicio1.php
@@ -0,0 +1,8 @@
+<!--1) Ejercicio01.php - 
+Crea un formulario que permita gesƟonar la canƟdad de refresco o
+leche que hay en un supermercado. (2 puntos)
+a) Se debe mantener el nombre del trabajador que está uƟlizando la aplicación.
+b) Se debe poder añadir y quitar leche o refresco seleccionando de una lista
+c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese
+caso mostrar error.
+ -->
