fd6cfce2aedd8dd02a48cee1ab2689495d61f03d
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2025-02-06 09:47:14 +0100

    traduce and modify the code

--- Sesiones-RA4/Ejercicio1.php
+++ Sesiones-RA4/Ejercicio1.php
@@ -1,147 +1,124 @@
-<!--1) Ejercicio01.php - 
-Crea un formulario que permita gesƟonar la canƟdad de refresco o
-leche que hay en un supermercado. (2 puntos)
-a) Se debe mantener el nombre del trabajador que está uƟlizando la aplicación.
-b) Se debe poder añadir y quitar leche o refresco seleccionando de una lista
-c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese
-caso mostrar error.
+
+ <!--1) Exercise1.php
+ Create a form that allows you to manage the amount of soda or milk in a supermarket. (2 points)
+a) The name of the worker using the application must be kept.
+b) You must be able to add and remove milk or soda by selecting from a list
+c) It must be controlled that more units cannot be removed than there are, in that
+case show error.
  -->
 
  <?php
- session_start(); 
-// when user click on a button(if form POST has been submitted )
-        
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-           // get data from form 
-            $worker = $_POST['worker'] ;
-             $product = $_POST['prodcut'];
-             $quantity = $_POST['quantity'];
-             
-            
-        // save worker data
-        $_SESSION['worker'] = $worker; 
-        // detect button
-        // to add products
-        if (isset($_POST['add'])) {
-            //evaluate product
-            switch ($product) {
-                case 'milk':
-                    $_SESSION['milk'] += $quantity;
-                    break;
-                
-                case 'soft_drink':
-                    $_SESSION['softDrink'] += $quantity;
-                    break;
-                default:
-                    echo "product not found";
-                    break;
-            }
-        }
-        
-            
-                
-            // add quantity to corresponding product
+session_start();
 
-            if (isset($_POST['add'])) {
-                switch ($product) {
-                    case 'milk':
-                        $_SESSION['milk']+=$quantity;
-                        break;
-                    case 'softDrink':
-                        $_SESSION['softDrink']+=$quantity;
-                        break;
-                    default:
-                        # code...
-                        break;
-                }
-
-            //to remove products
-
-        }elseif (isset($_POST['remove'])) {
- 
-            switch ($product) {
-                case 'milk':
-                    if ($_SESSION['milk'] - $quantity < 0) {
-                        echo "<br> <font color= 'red'> <p> Error: It is impossible remove more quantity than we have in store.</p></dont>";
-                    } else {
-                        $_SESSION['milk'] -= $quantity;
-                    }
-                    break;
-                case 'softDrink':
-                    if ($_SESSION['softDrink'] - $quantity < 0) {
-                        echo "<br> <font color= 'red'> <p> Error: It is impossible remove more quantity than we have in store.</p></dont>";
-                    } else {
-                        $_SESSION['softDrink'] -= $quantity;
-                    }                        break;
-                default:
-                echo "<br> <font color= 'red'> <p> Error: Product not found.</p></dont>";
-                    break;
-            }
-            }
+if (isset($_POST['reset'])) {
+    session_destroy();  //Eliminate all session data
+    header("Location: Ejercicio1.php");  //Recharge the page to clean the values
+    exit();
 }
 
-//initialize session variables
+//Initialize session variables if they do not exist
 if (!isset($_SESSION['worker'])) {
     $_SESSION['worker'] = '';
 }
 if (!isset($_SESSION['softDrink'])) {
     $_SESSION['softDrink'] = 0;
-    
 }
 if (!isset($_SESSION['milk'])) {
     $_SESSION['milk'] = 0;
 }
- 
 
+//send the form
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $worker = $_POST['worker'];
+    $product = $_POST['drink']; //corrects to "product"
+    $quantity = isset($_POST['quantity']) ? intval($_POST['quantity']) : 0;
 
+    //save the worker's name in the session
+    $_SESSION['worker'] = $worker;
 
- ?>
- <!DOCTYPE html>
- <html>
+    //add products
+    if (isset($_POST['add'])) {
+        switch ($product) {
+            case 'milk':
+                $_SESSION['milk'] += $quantity;
+                break;
+            case 'soft_drink':
+                $_SESSION['softDrink'] += $quantity;
+                break;
+            default:
+                echo "<p style='color:red;'>Error: Producto no encontrado.</p>";
+                break;
+        }
+    }
 
- <head>
-
-<title>Supermarket management</title>
-<style>
-
-.buttons{
-    margin-left: 45%;
-    margin-right: 35%;
-
+    //remove products
+    if (isset($_POST['remove'])) {
+        switch ($product) {
+            case 'milk':
+                if ($_SESSION['milk'] >= $quantity) {
+                    $_SESSION['milk'] -= $quantity;
+                } else {
+                    echo "<p style='color:red;'>Error: No puedes eliminar más unidades de las que hay.</p>";
+                }
+                break;
+            case 'soft_drink':
+                if ($_SESSION['softDrink'] >= $quantity) {
+                    $_SESSION['softDrink'] -= $quantity;
+                } else {
+                    echo "<p style='color:red;'>Error: No puedes eliminar más unidades de las que hay.</p>";
+                }
+                break;
+            default:
+                echo "<p style='color:red;'>Error: Producto no encontrado.</p>";
+                break;
+        }
+    }
 }
-.buttons button{
-    margin: 1px;
-}
+?>
 
-</style>
- </head>
+<!DOCTYPE html>
+<html>
 
- <body>
-    <h1 style="text-align:center">Supermarket management</h1>
-    <form action="Ejercicio1.php" method="POST"></form>
-    <Label for="worker"> Worker name:</Label>
-    <input type="text" id="worker" name="worker" value="<?php echo isset($_POST['worker'])?$_POST['worker'] :''; ?>"><br><br>
-    <h2 style="text-align:center">Choose product:</h2>
+<head>
+    <title>Supermarket Management</title>
+    <style>
+        .buttons {
+            margin-left: 45%;
+            margin-right: 35%;
+        }
+        .buttons button {
+            margin: 1px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Supermarket Management</h1>
+    <form action="Ejercicio1.php" method="POST">
+        <label for="worker">Worker name:</label>
+        <input type="text" id="worker" name="worker" value="<?php echo $_SESSION['worker']; ?>"><br><br>
+
+        <h2>Choose product:</h2>
         <select name="drink" id="drink">
             <option value="soft_drink">Soft Drink</option>
-            <option value="milk">milk</option>
+            <option value="milk">Milk</option>
         </select>
+
+        <h2>Product quantity:</h2>
+        <input type="number" id="quantity" name="quantity" min="1" required><br><br>
+
+        <input type="submit" value="Add" name="add">
+        <input type="submit" value="Remove" name="remove">
         
-
-    <h2 style="text-align:center">Product quantity:</h2>
-
-        <input type="number" id="quantity" name="quantity" min="1" ><br><br>
-        <input type="submit" value="add" name="add">
-        <input type="submit" value="remove" name="remove">
-        <input type="reset" value="reset">>
+    </form>
+    <form action="Ejercicio1.php" method="POST">
+    <input type="submit" value="Reset" name="reset">
     </form>
 
-    <h2 style="text-align:center">Inventary:</h2>
-    <p>worker: <?php echo isset($_SESSION['worker'])? $_SESSION['worker'] : '';?></ph>
-    <p>units milk: <?php isset($_SESSION['milk'])? $_SESSION['milk'] : '';?></p>
-    <p>units soft drink: <?php isset($_SESSION['softDrink'])? $_SESSION['softDrink'] : '';?></p>
-<!-- form with 3 buttons -->
-<!-- list values worjer,milk,softdrink... -->
- </body>
+    <h2>Inventory:</h2>
+    <p>Worker: <?php echo $_SESSION['worker']; ?></p>
+    <p>Units of Milk: <?php echo $_SESSION['milk']; ?></p>
+    <p>Units of Soft Drink: <?php echo $_SESSION['softDrink']; ?></p>
+</body>
 
- </html>
+</html>
